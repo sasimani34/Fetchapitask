873d03fa19235efa4d2018de80b8658d926cf080
sasimani34 <sasidinesh01@gmail.com>
2023-10-21 12:02:46 +0530

    first commit

---
+++ Login/loginscript.js
@@ -0,0 +1,27 @@
+
+  /*................login form.....................*/
+  const loginForm = document.querySelector('#loginform')
+  const loginuname = document.querySelector('#lusername');
+  const loginpwd = document.querySelector('#lpassword');
+  
+  loginForm.addEventListener('submit',(event)=>{
+      event.preventDefault()
+      loginValidateInputs()
+  })
+  
+  function loginValidateInputs(){
+      const usernameVal = loginuname.value.trim()
+      const passwordVal = loginpwd.value.trim()
+      const getUname = localStorage.getItem("username");
+      const getPassword = localStorage.getItem("password");
+      
+      if (usernameVal === getUname && passwordVal === getPassword) {
+          alert("Login Successfully!");
+          window.location.href = "todolist.html";
+      } else {
+          alert("Invalid username or password.");
+      }
+  
+      
+  }
+  
